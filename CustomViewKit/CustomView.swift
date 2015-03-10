75dd2547febfb8ed806c1e75da099f253a0df8a4
Max Beizer <max.beizer@gmail.com>
2015-03-09 19:11:35 -0500

    Initial commit

---
+++ CustomViewKit/CustomView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomView.swift
+//  Sights
+//
+//  Created by Max Beizer on 3/9/15.
+//  Copyright (c) 2015 Max Beizer. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: NSObject {
+   
+}
---
+++ CustomViewKit/CustomViewKit.h
@@ -0,0 +1,19 @@
+//
+//  CustomViewKit.h
+//  CustomViewKit
+//
+//  Created by Max Beizer on 3/9/15.
+//  Copyright (c) 2015 Max Beizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomViewKit.
+FOUNDATION_EXPORT double CustomViewKitVersionNumber;
+
+//! Project version string for CustomViewKit.
+FOUNDATION_EXPORT const unsigned char CustomViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomViewKit/PublicHeader.h>
+
+
